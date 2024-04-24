e1468ae19ebc1ef6f1f77bc0e4b2bce974eff1ff
najia01 <validress@outlook.fr>
2024-04-24 23:32:59 +0200

    affichage ofrre

---
+++ src/Controller/JobController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Offre;
+use App\Repository\OffreRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class JobController extends AbstractController
+{
+    #[Route('/', name: 'app_job')]
+    public function index(OffreRepository $offreRepository): Response
+    {
+        return $this->render('job/index.html.twig', [
+            'offres' => $offreRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/ {id}', name: 'app_job_show')]
+    public function show(Offre $offre): Response
+    {
+        
+        return $this->render('job/show.html.twig', [
+            'offre' => $offre
+        ]);
+    }
+}
